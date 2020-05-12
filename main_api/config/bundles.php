2c0e991d486fdf356eff26c16c1730ec295cbfbb
Alex Vinnikov <vin-nikov@yandex.ru>
2020-05-13 00:46:47 +0300

    add cors rules

--- main_api/config/bundles.php
+++ main_api/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
